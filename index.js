c8f2366c89cf40da9eb16b8eea579aed48cb6650
NtGZeko <matheooz@live.fr>
2023-11-14 11:32:30 +0100

    node init

---
+++ index.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk');
+
+console.log(
+  chalk.blue('Lou') +
+    ' ' +
+    chalk.red('maxence') +
+    ' ' +
+    chalk.green('Laura') +
+    ' ' +
+    chalk.yellow('gael')
+);
